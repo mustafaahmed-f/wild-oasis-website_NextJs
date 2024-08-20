74d68e4661034722591712ade3a7eeb56a04664e
Mustafa Ahmed <mostafafikry97@gmail.com>
2024-08-20 18:37:05 +0300

    after addding cabins

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect } from "react";
 import CabinData from "./CabinData";
 import { Metadata } from "next";
-
-interface pageProps {}
+import { getCabins } from "@/app/_lib/data-service";
+import CabinCard from "@/app/_Components/CabinCard";
 
 export const metadata: Metadata = {
   title: "Cabins",
 };
 
-const page: React.FC<pageProps> = async ({}) => {
-  const cabins: any[] = [];
-
+async function page() {
+  const cabins: any[] = await getCabins();
   return (
     <div>
       <h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -24,8 +23,16 @@ const page: React.FC<pageProps> = async ({}) => {
         home away from home. The perfect spot for a peaceful, calm vacation.
         Welcome to paradise.
       </p>
+
+      {cabins.length > 0 ? (
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+          {cabins.map((cabin: any) => (
+            <CabinCard cabin={cabin!} key={cabin.id} />
+          ))}
+        </div>
+      ) : null}
     </div>
   );
-};
+}
 
 export default page;
